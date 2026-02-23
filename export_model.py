4e3332853ee3629620ceaf4657fced11cb6e29eb
abhikunwar <78917795+abhikunwar@users.noreply.github.com>
2026-02-23 13:39:08 +0530

    Update export_model.py

--- export_model.py
+++ export_model.py
@@ -1,17 +1,14 @@
-import mlflow
-import os
 import sys
+import mlflow
 
-# CLI args
-model_name = sys.argv[1] if len(sys.argv) > 1 else "dev-version1"
-model_stage = sys.argv[2] if len(sys.argv) > 2 else "champion"
+model_name = sys.argv[1]
+model_stage = sys.argv[2]
 
 model_uri = f"models:/{model_name}/{model_stage}"
+
 print(f"Downloading model from MLflow: {model_uri}")
 
-# Download artifacts to local folder
-docker_model_path = "model"
-os.makedirs(docker_model_path, exist_ok=True)
-mlflow.artifacts.download_artifacts(model_uri, docker_model_path)
-
-print(f"Model saved to: {docker_model_path}")
+mlflow.artifacts.download_artifacts(
+    artifact_uri=model_uri,
+    dst_path="model"
+)
