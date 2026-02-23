275de00d84798681d47268a645d9f86a7de4bc7a
abhikunwar <78917795+abhikunwar@users.noreply.github.com>
2026-02-23 13:36:50 +0530

    Change default model name and stage in export_model.py

--- export_model.py
+++ export_model.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 # CLI args
-model_name = sys.argv[1] if len(sys.argv) > 1 else "my_model"
-model_stage = sys.argv[2] if len(sys.argv) > 2 else "Champion"
+model_name = sys.argv[1] if len(sys.argv) > 1 else "dev-version1"
+model_stage = sys.argv[2] if len(sys.argv) > 2 else "champion"
 
 model_uri = f"models:/{model_name}/{model_stage}"
 print(f"Downloading model from MLflow: {model_uri}")
