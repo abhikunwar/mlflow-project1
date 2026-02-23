71d09f3f4ccdccfe59f29360a57892f6bb2c7920
abhikunwar <78917795+abhikunwar@users.noreply.github.com>
2026-02-23 13:53:50 +0530

    Update export_model.py

--- export_model.py
+++ export_model.py
@@ -1,11 +1,13 @@
 import sys
-import mlflow
 import os
-mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
-model_name = sys.argv[1]
-model_stage = sys.argv[2]
+import mlflow
 
-model_uri = f"models:/{model_name}/{model_stage}"
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+
+model_name = sys.argv[1]
+model_alias = sys.argv[2]
+
+model_uri = f"models:/{model_name}@{model_alias}"
 
 print(f"Downloading model from MLflow: {model_uri}")
 
